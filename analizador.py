1d71bee16753be00cd2bd335829ca1161ce3ac98
Rodolfo <rodomaxi2@gmail.com>
2021-10-07 14:07:09 -0500

    Cambios en la estructura del proyecto

--- analizador.py
+++ analizador.py
@@ -1,9 +1,157 @@
+from bs4 import BeautifulSoup
+import requests
+from requests.exceptions import ConnectionError
+import re
+import os
 from tkinter import *
 import tkinter.ttk as ttk
-from main import *
+# from main import *
 
-link = 'https://docs.python.org/3.8/library/tkinter.html'
+# Logica y algoritmo
+directorio = 'textDir'
 
+estado = "Listo para trabajar"
+
+contadorGlobal = 0
+
+contadorAltas = 0
+contadorMedias = 0
+contadorBajas = 0
+
+palabrasPrueba = ['ocio']
+alta = ['violacion', 'misoginia', 'pornografia']
+media = ['sexo']
+baja = ['pendejo']
+
+# Esta funcion cuenta las palabras dentro de un texto que coincidan con el array dado
+
+
+def actualizarEstado(estado, label):
+    pass
+
+
+def contarPalabras(texto, palabras):
+    contador = 0
+    for palabra in palabras:
+        print(palabra)
+        coincidencias = re.findall(r'\w*' + palabra + '', texto)
+        contador += len(coincidencias)
+    return contador
+
+# Esta funcion borra todos los archivos de ejecuciones pasadas
+
+
+def eraseCache():
+    for root, dirs, files in os.walk(directorio):
+        for name in files:
+            os.unlink(os.path.join(root, name))
+
+
+def formatString(link):
+    link = link.replace('<title>', '')
+    link = link.replace('</title>', '')
+    link = link.replace(' ', '')
+    link = link.replace('|', '')
+    link = link.replace('?', '')
+    link = link.replace('¿', '')
+    link = link.replace('\"', '')
+    link = link.replace('\\', '')
+    link = link.replace('/', '')
+    link = link.replace('\n', '')
+    return link
+
+
+def extractText(link, directorio):
+    contadorGlobal = 0
+
+    contadorAltas = 0
+    contadorMedias = 0
+    contadorBajas = 0
+    try:
+        response = requests.get(link, timeout=(3, 27))
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            if(soup.title == None):
+                # Si el link actual no tiene una etiqueta de titulo, el titulo sera el propio link formateado
+                title = link
+                title = formatString(title)
+            else:
+                try:
+                    title = soup.title.string
+
+                except ValueError as ve:
+                    print('El titulo no contiene el atributo string', ve)
+                    return
+
+            contadorGlobal += contarPalabras(title, palabrasPrueba)
+            for text in soup.stripped_strings:
+                contadorGlobal = contarPalabras(text, palabrasPrueba)
+            # Por cada una de las noticias se creara un archivo con el contenido siguiente
+            # with open(f'{directorio}/{title}.txt', 'w', encoding='utf-8') as f:
+            #     f.write(title)
+            #     contadorGlobal += contarPalabras(title, palabrasPrueba)
+            #     print(contadorGlobal)
+            #     for text in soup.stripped_strings:
+            #         f.write(text)
+            #         f.write('\n')
+            #         contadorGlobal = contarPalabras(text, palabrasPrueba)
+
+            # print(contadorGlobal)
+
+        else:
+            return
+            # raise ValueError(f'Error: {response.status_code} con link: {link}')
+
+        response.close()
+    except requests.exceptions.Timeout:
+        print('El tiempo se agoto')
+
+    except ConnectionError:
+        print('Link caido')
+
+
+def searchLinks(link):
+    try:
+        response = requests.get(link)  # se accede a la pagina principal
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            estado = "Trabajando..."
+
+            if not os.path.isdir(directorio):
+                os.mkdir(directorio)
+            else:
+                eraseCache()
+
+            for newLink in soup.find_all('a'):
+                try:
+                    if(newLink.get('href')):
+                        parsedLink = newLink.get('href').replace(' ', '')
+                        #print("se encontro el link: ", parsedLink)
+                        if parsedLink.find('_blank') > 0:
+                            continue
+                        elif parsedLink.find('sic') > 0:
+                            continue
+                        elif parsedLink[0:4] == 'http':
+                            pass
+                        else:
+                            parsedLink = link + parsedLink
+
+                        print(parsedLink)
+                        extractText(parsedLink, directorio)
+                    else:
+                        print('Link no valido')
+
+                except ValueError as ve:
+                    print(ve)
+
+        else:
+            raise ValueError(f'Error {response.status_code}')
+    except ValueError as ve:
+        print('Un error ocurrio:', ve)
+
+
+# Interfaz de usuario e interaccion
 window = Tk()
 window.title("Analizador de paginas")
 window.geometry("400x400")
